28df9d24c511e18f92da25461ea5179501b892e2
Hieu Truong Xuan <truongxuanhieu11@gmail.com>
2025-07-25 08:09:11 +0000

    Fix name typo

--- auto_run.sh
+++ auto_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ./build/cabw_enc datasets/harwell_boeing/A-pores_1.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
-./build/cabw_enc datasets/harwell_boeing/B_ibm32.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
+./build/cabw_enc datasets/harwell_boeing/B-ibm32.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/C-bcspwr01.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/D-bcsstk01.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/E-bcspwr02.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/F-curtis54.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
-./build/cabw_enc datasets/harwell_boeing/G-wil57.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
+./build/cabw_enc datasets/harwell_boeing/G-will57.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/H-impcol_b.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/I-ash85.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
 ./build/cabw_enc datasets/harwell_boeing/J-nos4.mtx.rnd --ladder -limit-real-time 150 -limit-memory 30000
