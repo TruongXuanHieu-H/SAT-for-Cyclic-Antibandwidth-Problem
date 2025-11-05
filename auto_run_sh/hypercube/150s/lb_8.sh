d64f99c651b2488144dd28f1e382598d3143951e
Hiếu Hay Ho <truongxuanhieu11@gmail.com>
2025-11-05 16:02:30 +0700

    ./sh files for hypercube

---
+++ auto_run_sh/hypercube/150s/lb_8.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TIME_LIMIT=150
+MEMORY_LIMIT=30000
+SYMMETRY_BREAK="highest-degree"
+WORKER_COUNT=8
+ENC_DIR="../../../build"
+DATASET_DIR="../../../datasets/conjecture_optimal/hypercube"
+LOG_DIR="../../../logs_150s/conjecture_optimal/hypercube/binarysearchfromLB_highestdegree_8workers"
+ADD_CONFIGS="--binary-search-from-lb"
+
+mkdir -p "$LOG_DIR"
+
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_4_16.txt      --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_4_16.log
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_5_32.txt      --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_5_32.log
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_6_64.txt      --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_6_64.log
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_7_128.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_7_128.log
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_8_256.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_8_256.log
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_9_512.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_9_512.log
+$ENC_DIR/cabw_enc $DATASET_DIR/hypercube_10_1024.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/hypercube_10_1024.log
