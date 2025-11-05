0f349fd810df59620b323e1a4e46cda94026ae5b
Hiếu Hay Ho <truongxuanhieu11@gmail.com>
2025-11-05 15:19:04 +0700

    fix variable error in .sh files

--- auto_run_sh/3d_mesh/150s/lb_4.sh
+++ auto_run_sh/3d_mesh/150s/lb_4.sh
@@ -5,52 +5,30 @@ MEMORY_LIMIT=30000
 SYMMETRY_BREAK="highest-degree"
 WORKER_COUNT=4
 ENC_DIR="../../../build"
-DATASET_DIR="../../../datasets/unknow_optimal/random_generated_graph_2013"
-LOG_DIR="../../../logs_150s/unknow_optimal/random_generated_graph_2013/binarysearchfromLB_highestdegree_4workers"
+DATASET_DIR="../../../datasets/conjecture_optimal/3dmesh"
+LOG_DIR="../../../logs_150s/conjecture_optimal/3dmesh/binarysearchfromLB_highestdegree_4workers"
 ADD_CONFIGS="--binary-search-from-lb"
 
 mkdir -p "$LOG_DIR"
 
-$ENC_DIR/cabw_enc $DATASET_DIR/p1_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p1_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p2_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p2_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p3_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p3_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p4_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p4_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p5_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p5_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p6_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p6_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p7_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p7_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p8_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p8_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p9_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p9_100_200.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p10_100_200 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p10_100_200.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_2_2_3.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_2_2_3.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_2_2_168.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_2_2_168.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_2_2_335.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_2_2_335.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_2_2_500.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_2_2_500.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_3_3_3.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_3_3_3.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_3_3_135.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_3_3_135.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_3_3_270.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_3_3_270.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_3_3_400.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_3_3_400.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_4_4_5.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_4_4_5.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_4_4_68.txt    --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_4_4_68.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_4_4_137.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_4_4_137.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_4_4_200.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_4_4_200.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_5_5_7.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_5_5_7.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_5_5_35.txt    --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_5_5_35.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_5_5_70.txt    --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_5_5_70.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_5_5_100.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_5_5_100.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_6_6_8.txt     --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_6_6_8.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_6_6_36.txt    --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_6_6_36.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_6_6_72.txt    --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_6_6_72.log
+$ENC_DIR/cabw_enc $DATASET_DIR/3dmesh_6_6_100.txt   --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/3dmesh_6_6_100.log
 
-$ENC_DIR/cabw_enc $DATASET_DIR/p11_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p11_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p12_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p12_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p13_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p13_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p14_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p14_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p15_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p15_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p16_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p16_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p17_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p17_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p18_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p18_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p19_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p19_100_600.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p20_100_600 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p20_100_600.log
-
-$ENC_DIR/cabw_enc $DATASET_DIR/p21_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p21_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p22_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p22_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p23_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p23_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p24_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p24_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p25_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p25_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p26_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p26_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p27_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p27_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p28_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p28_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p29_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p29_200_400.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p30_200_400 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p30_200_400.log
-
-$ENC_DIR/cabw_enc $DATASET_DIR/p31_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p31_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p32_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p32_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p33_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p33_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p34_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p34_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p35_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p35_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p36_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p36_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p37_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p37_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p38_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p38_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p39_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p39_200_2000.log
-$ENC_DIR/cabw_enc $DATASET_DIR/p40_200_2000 --ladder -limit-real-time $TIME_LIMIT -limit-memory $MEMORY_LIMIT -symmetry-break $SYMMETRY_BREAK -worker-count $WORKER_COUNT $ADD_CONFIGS 2>&1 | tee $LOG_DIR/p40_200_2000.log
